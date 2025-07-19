61752ee29f086a1da50a24752e59bdd2b2ce6847
liecn <chenningli2019@gmail.com>
2025-07-19 16:04:57 -0400

    remove build

--- astra-sim-alibabacloud/build/simai_flowsim/build/AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/SharedBusStat.cc.o.d
+++
@@ -1,3 +0,0 @@
-AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/SharedBusStat.cc.o: \
- /data1/lichenni/projects/SimAI/astra-sim-alibabacloud/astra-sim/system/SharedBusStat.cc \
- /usr/include/stdc-predef.h
--- astra-sim-alibabacloud/build/simai_flowsim/build/AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/StatData.cc.o.d
+++
@@ -1,3 +0,0 @@
-AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/StatData.cc.o: \
- /data1/lichenni/projects/SimAI/astra-sim-alibabacloud/astra-sim/system/StatData.cc \
- /usr/include/stdc-predef.h
